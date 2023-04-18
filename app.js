fc01dd85d703899ab764a5dd036ec07fdec4a039
Ewan Sheldon <ewansheldon@gmail.com>
2023-04-18 15:48:55 +0200

    move some things into env vars, to avoid multiple redeploys later

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ const Web3 = require('web3');
 const fs = require('fs');
 require('dotenv').config();
 const schedule = require('node-schedule');
-const web3 = new Web3(`https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`);
+const networkName = process.env.NETWORK_NAME || 'mainnet';
+const web3 = new Web3(`https://${networkName}.infura.io/v3/${process.env.INFURA_API_KEY}`);
 const contracts = JSON.parse(fs.readFileSync('contracts.json', { encoding: 'utf8' }));
-const tokenManagerContract = new web3.eth.Contract(contracts.tokenManager.abi, contracts.tokenManager.address);
-const minsPerDay = 60 * 24;
-const dataPoints = 48;
-const intervalMins = minsPerDay / dataPoints;
+const tokenManagerContract = new web3.eth.Contract(contracts.tokenManager, process.env.TOKEN_MANAGER_ADDRESS);
 
 let prices = {};
 
@@ -18,27 +16,36 @@ app.get('/', async (_, res) => {
   res.json(prices);
 });
 
-const addPriceToData = token => {
+const addPriceToData = (token, ts) => {
   const symbol = web3.utils.hexToUtf8(token.symbol);
-  const chainlinkContract = new web3.eth.Contract(contracts.chainlink.abi, token.clAddr);
+  const chainlinkContract = new web3.eth.Contract(contracts.chainlink, token.clAddr);
   chainlinkContract.methods.latestRoundData().call().then(data => {
     if (prices[symbol]) {
-      prices[symbol].prices = [ ...prices[symbol].prices, data.answer ].splice(-dataPoints, dataPoints);
+      prices[symbol].prices = [ 
+        ...prices[symbol].prices, {
+          price: data.answer,
+          ts: ts
+        }
+      ].splice(-48, 48);
     } else {
       chainlinkContract.methods.decimals().call().then(dec => {
         prices[symbol] = {
           decimals: dec,
-          prices: [data.answer]
+          prices: [{
+            price: data.answer, 
+            ts: ts
+          }]
         }
       });
     }
   });
 }
 
-schedule.scheduleJob(`*/${intervalMins} * * * *`, _ => {
+schedule.scheduleJob(`0,30 * * * *`, _ => {
+  const ts = Math.floor(new Date() / 1000);
   tokenManagerContract.methods.getAcceptedTokens().call().then(tokens => {
     tokens.map(token => {
-      addPriceToData(token);
+      addPriceToData(token, ts);
     });
   });
 });
