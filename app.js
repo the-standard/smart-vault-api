17a176c52850be202f276260c4eaae18f9bb7b17
Ewan Sheldon <ewansheldon@gmail.com>
2023-05-24 16:29:28 +0200

    add request method to api logger

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ schedule.scheduleJob(`0,30 * * * *`, _ => {
 });
 
 const server = http.createServer((req, res) => {
-  console.log(`${new Date().toISOString()} | ${req.url}`);
+  console.log(`${new Date().toISOString()} | ${req.method} ${req.url}`);
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify(prices));
 });
