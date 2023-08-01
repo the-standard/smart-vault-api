bdf36cedebd7a046fe2ab9c9a6a4a904d138b15b
Ewan Sheldon <ewansheldon@gmail.com>
2023-08-01 15:18:50 +0200

    only run liquidations for arbitrum one

--- src/liquidation.js
+++ src/liquidation.js
@@ -1,20 +1,19 @@
 const schedule = require('node-schedule');
 const { getContract } = require("./contractFactory");
 const { ethers } = require('ethers');
-const { getNetworks } = require('./networks');
+const { getNetwork } = require('./networks');
 
 const scheduleLiquidation = async _ => {
-  getNetworks().forEach(async network => {
-    schedule.scheduleJob('*/5 * * * *', async _ => {
-      try {
-        const provider = new ethers.getDefaultProvider(network.rpc)
-        const wallet = new ethers.Wallet(process.env.WALLET_PRIVATE_KEY, provider);
-        await (await getContract(network.name, 'SmartVaultManager')).connect(wallet).liquidateVaults()
-        console.log(network.name, 'vault-liquidated');
-      } catch(e) {
-        console.log(network.name, e.reason)
-      }
-    });
+  const network = getNetwork('arbitrum');
+  schedule.scheduleJob('* * * * *', async _ => {
+    try {
+      const provider = new ethers.getDefaultProvider(network.rpc)
+      const wallet = new ethers.Wallet(process.env.WALLET_PRIVATE_KEY, provider);
+      await (await getContract(network.name, 'SmartVaultManager')).connect(wallet).liquidateVaults()
+      console.log(network.name, 'vault-liquidated');
+    } catch(e) {
+      console.log(network.name, e.reason)
+    }
   });
 }
 
--- src/networks.js
+++ src/networks.js
@@ -1,20 +1,27 @@
+const networks = [
+  {
+    name: 'arbitrum',
+    rpc: 'https://arb1.arbitrum.io/rpc'
+  },
+  {
+    name: 'sepolia',
+    rpc: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`
+  },
+  {
+    name: 'arbitrum_goerli',
+    rpc: 'https://goerli-rollup.arbitrum.io/rpc'
+  }
+]
+
 const getNetworks = _ => {
-  return [
-    {
-      name: 'arbitrum',
-      rpc: 'https://arb1.arbitrum.io/rpc'
-    },
-    {
-      name: 'sepolia',
-      rpc: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`
-    },
-    {
-      name: 'arbitrum_goerli',
-      rpc: 'https://goerli-rollup.arbitrum.io/rpc'
-    }
-  ]
+  return networks;
+}
+
+const getNetwork = name => {
+  return networks.filter(network => network.name === name)[0];
 }
 
 module.exports = {
-  getNetworks
+  getNetworks,
+  getNetwork
 }
