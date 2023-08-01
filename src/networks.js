5e16342cacaa3ec2a47953bd13db35b8806be981
Ewan Sheldon <ewansheldon@gmail.com>
2023-08-01 15:12:34 +0200

    add arbitrum one to price checks

--- src/networks.js
+++ src/networks.js
@@ -1,6 +1,10 @@
 const getNetworks = _ => {
   return [
     {
+      name: 'arbitrum',
+      rpc: 'https://arb1.arbitrum.io/rpc'
+    },
+    {
       name: 'sepolia',
       rpc: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`
     },
