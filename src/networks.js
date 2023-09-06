ce759411de083a129ff976117c487ebfde1858ae
Ewan Sheldon <ewansheldon@gmail.com>
2023-09-06 11:24:51 +0200

    remove sepolia

--- src/networks.js
+++ src/networks.js
@@ -4,10 +4,6 @@ const networks = [
     rpc: 'https://arb1.arbitrum.io/rpc'
   },
   {
-    name: 'sepolia',
-    rpc: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`
-  },
-  {
     name: 'arbitrum_goerli',
     rpc: 'https://goerli-rollup.arbitrum.io/rpc'
   }
