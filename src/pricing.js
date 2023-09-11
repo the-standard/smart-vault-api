ebb249d42c3e24a083e6dd23af9b8564cb8848ad
Ewan Sheldon <ewansheldon@gmail.com>
2023-09-11 13:43:23 +0200

    log redis port

--- src/pricing.js
+++ src/pricing.js
@@ -5,7 +5,6 @@ require('./networks')
 
 const redisHost = process.env.REDIS_HOST || '127.0.0.1';
 const redisPort = process.env.REDIS_PORT || '6379';
-console.log(redisHost)
 
 const redis = createClient({
   url: `redis://${redisHost}:${redisPort}`
