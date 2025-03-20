b3276eacdf66321513f55c440a22072702401ce7
Ewan Sheldon <ewansheldon@gmail.com>
2025-03-20 13:23:50 +0100

    look for usdt special character and replace it

--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,7 @@ const parseQueryParams = queryParams => {
   if (queryParams) {
     params = queryParams && queryParams.split('&').reduce((obj, param) => {
       const splitParam = param.split('=');
-      return { ... obj, [splitParam[0]]: splitParam[1] }
+      return { ... obj, [splitParam[0]]: splitParam[1].replace("%E2%82%AE", "₮") };
     }, params);
   }
 
