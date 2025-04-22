bdab50ca7fb94ff9112f195b8323a057ffcc88f6
Ewan Sheldon <ewansheldon@gmail.com>
2025-04-22 12:30:44 +0200

    fetch txs from the graph v0.0.10

--- src/transactions.js
+++ src/transactions.js
@@ -1,13 +1,10 @@
+const https = require('https')
 const { redisClient } = require('./redis');
 const { parseQueryParams } = require('./utils')
 
 const vaultTransactionsAddress = url => {
-  const regex = /^\/transactions\/(?<address>0x[\w\d]*)(\?(?<queryParams>.*))?.*$/;
+  const regex = /^\/transactions\/(?<address>0x[\w\d]*)(\/?(?<detailID>0x[\w\d\-]*))?(\?(?<queryParams>.*))?.*$/;
   return url.match(regex) && url.match(regex).groups;
-}
-
-const getTransactionsKey = address => {
-  return `vaultTxs:${address.toLowerCase()}`;
 }
   
 const parsedQueryParamsWithDefaults = queryParams => {
@@ -20,46 +17,63 @@ const parsedQueryParamsWithDefaults = queryParams => {
   return params;
 }
 
-const formatTransactions = transactionData => {
-  // SCHEMA:
-  // key: 'vaultTxs:0x...'
-  // score: timestamp
-  // value: 'type:hash:blockNo:asset:amount:amountDec:minted:collateralValue'
-  // e.g. 'deposit:0x8ae26a528861d3e6c08d4331885eaba2d1b5b55fc540234fc9b8c9c198a0d429:124132949:PAXG:8000000000000000:18:136175000000000000000:181087962079756018667'
-  const schema = ['type','txHash','blockNo','asset','amount','assetDec','minted','totalCollateralValue'];
+const fields = {
+  usdsMinted: 'to amount fee',
+  usdsBurned: 'amount fee',
+  collateralRemoved: 'symbol to amount',
+  assetRemoved: 'token to amount',
+  collateralDeposited: 'token from amount'
+}
 
-  return transactionData.map(data => {
-    const labelledData = {
-      timestamp: data.score
-    };
-    return data.value.split(':').reduce((obj, item, i) => {
-      return {
-        ... obj,
-        [schema[i]]: item
+const fetchVaultActivityData = async (address, detailID, parameters) => {
+  const query = parameters && parameters.detailType && detailID ?
+    `query { ${parameters.detailType}(id: "${detailID}") { id ${fields[parameters.detailType]} } }` :
+    `query { smartVaultActivities(where: { vault: "${address.toLowerCase()}" }) { id vault{id} user detailType blockTimestamp transactionHash minted totalCollateralValue } }`;
+  const dataString = JSON.stringify({ query });
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': dataString.length,
+    },
+    timeout: 2000
+  }
+
+  return new Promise((resolve, reject) => {
+    const req = https.request('https://api.studio.thegraph.com/query/109184/smart-vault-history/v0.0.10', options, (res) => {
+      if (res.statusCode < 200 || res.statusCode > 299) {
+        return reject(new Error(`HTTP status code ${res.statusCode}`))
       }
-    }, labelledData);
-  });
+
+      const body = []
+      res.on('data', (chunk) => body.push(chunk))
+      res.on('end', () => {
+        const resString = Buffer.concat(body).toString()
+        resolve(resString)
+      })
+    })
+
+    req.on('error', (err) => {
+      reject(err)
+    })
+
+    req.on('timeout', () => {
+      req.destroy()
+      reject(new Error('Request time out'))
+    })
+
+    req.write(dataString)
+    req.end()
+  })
 }
 
 const getTransactions = async url => {
-  const {address, queryParams} = vaultTransactionsAddress(url);
-  const { page, limit, sort } = parsedQueryParamsWithDefaults(queryParams);
-  const start = (page - 1) * limit;
-  const end = start + limit - 1;
-  const REV = !(sort === 'asc');
-  const key = getTransactionsKey(address)
-  const transactionData = await redisClient.ZRANGE_WITHSCORES(key, start, end, {REV});
-  const count = await redisClient.ZCARD(key);
-  const transactions = formatTransactions(transactionData);
-  return {
-    data: transactions,
-    pagination: {
-      currentPage: page,
-      totalRows: count,
-      rowsPerPage: limit
-    }
-  }
-};
+  const { address, detailID, queryParams } = vaultTransactionsAddress(url);
+  const parameters = parsedQueryParamsWithDefaults(queryParams);
+  const data = await fetchVaultActivityData(address, detailID, parameters);
+  return JSON.parse(data).data;
+}
 
 module.exports = {
   getTransactions,
