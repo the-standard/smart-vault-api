e9a1f47e1e8959e43636e04c34959688317a95bc
Ewan Sheldon <ewansheldon@gmail.com>
2025-04-25 17:18:04 +0200

    subgraph v1.2.0

--- src/transactions.js
+++ src/transactions.js
@@ -26,7 +26,8 @@ const fields = {
   vaultLiquidated: '',
   swap: 'tokenIn tokenOut amountIn feeRate',
   depositYield: 'tokenIn stablePercentage',
-  withdrawYield: 'hypervisor tokenOut'
+  withdrawYield: 'hypervisor tokenOut',
+  autoRedemption: 'token hypervisor USDsRedeemed'
 }
 
 const post = async query => {
@@ -42,7 +43,7 @@ const post = async query => {
   }
 
   return new Promise((resolve, reject) => {
-    const req = https.request('https://api.studio.thegraph.com/query/109184/smart-vault-history/v1.1.6', options, (res) => {
+    const req = https.request('https://api.studio.thegraph.com/query/109184/smart-vault-history/v1.2.0', options, (res) => {
       if (res.statusCode < 200 || res.statusCode > 299) {
         return reject(new Error(`HTTP status code ${res.statusCode}`))
       }
